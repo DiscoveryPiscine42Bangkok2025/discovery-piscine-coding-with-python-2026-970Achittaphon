a575650a12fcc462d0903f205a016fc748eb4811
FroySumkham <froysumkham@gmail.com>
2026-02-12 16:01:30 +0700

    add ex01 and ex02

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+print("Enter a number")
+
+n = int(input())
+
+for i in range(10):
+    print(f"{i} X {n} = {i * n}")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+input("What you gotta say? : ")
+
+while True:
+    n = input("I got that! Anything else? : ")
+    
+    if (n == 'STOP'):
+        break
